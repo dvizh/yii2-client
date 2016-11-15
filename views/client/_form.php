c62b1d7210fae0c2b58bd7883fcaccba921ddeab
Ilya <php@php.name>
2016-11-15 15:20:29 +0500

    fix

--- views/client/_form.php
+++ views/client/_form.php
@@ -61,7 +61,7 @@ use pistol88\promocode\widgets\AddNew;
         </div>
         <div class="col-lg-2 col-md-3 col-xs-6">
             <?php if(yii::$app->has('organization') && $organization = yii::$app->get('organization')) { ?>
-                <?php echo $form->field($model, 'organization_id')->dropDownList(array_merge(['0' => 'Нет'], ArrayHelper::map($organization->getList(), 'id', 'name'))) ?>
+                <?php echo $form->field($model, 'organization_id')->dropDownList(array_merge(['' => 'Нет'], ArrayHelper::map($organization->getList(), 'id', 'name'))) ?>
             <?php } ?>
         </div>
     </div>
